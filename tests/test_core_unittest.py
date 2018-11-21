5f6e418326c6b639ddcb1193c309c552e19200da
danoscarmike <omeara.dan@gmail.com>
2018-11-21 14:30:28 -0800

    add tests, LICENSE

---
+++ tests/test_core_unittest.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append('../waqi-python')
+
+import unittest
+
+from waqi_python import core as waqi
+
+
+class TestCore(unittest.TestCase):
+
+    def test_get_city_feed(self):
+        response, payload = waqi.get_city_feed('shanghai')
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+        self.assertTrue(len(payload['data']) > 0, 'Should contain data.')
+
+    def test_get_local_feed(self):
+        response, payload = waqi.get_local_feed()
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+
+    def test_get_feed_by_location(self):
+        # Pass arbitrary location (in Ireland)
+        # Expect response['data'] to be populated
+        response, payload = waqi.get_feed_by_location(53.1, -7.4)
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+        self.assertTrue(len(payload['data']) > 0, 'Should contain data.')
+
+    def test_get_stations_in_bounds(self):
+        # Pass arbitrary location (bounding box of New South Wales, Australia)
+        # Expect response['data'] to be populated
+        response, payload = waqi.get_stations_in_bounds(-37.6, 140.5,
+                                                        -27.9, 154.1)
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+        self.assertTrue(len(payload['data']) > 0, 'Should contain data.')
+
+
+    def test_get_stations_in_bounds_expect_empty(self):
+        # Pass arbitrary location (bounding box in Coral Sea)
+        # Expect response['data'] to be empty
+        response, payload = waqi.get_stations_in_bounds(-27.9, 154.1,
+                                                        -27.7, 154.2)
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+        self.assertTrue(len(payload['data']) == 0, 'Payload should be empty.')
+
+
+    def test_search_stations(self):
+        response, payload = waqi.search_stations('san francisco')
+        self.assertEqual(response.status_code, 200, 'Should be 200.')
+        self.assertTrue('status' in payload, 'Payload should include "status".')
+        self.assertTrue('data' in payload, 'Payload should include "data".')
+        self.assertTrue(len(payload['data']) > 0, 'Payload should be empty.')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- waqi_python/core.py
+++ waqi_python/core.py
@@ -11,27 +11,27 @@ def _url(path):
 
 def get_city_feed(city):
     r = requests.get(_url(f'feed/{city}/?token={TOKEN}'))
-    return r.json()
+    return r, r.json()
 
 
 def get_local_feed():
     r = requests.get(_url(f'feed/here/?token={TOKEN}'))
-    return r.json()
+    return r, r.json()
 
 
 def get_feed_by_location(lat, lng):
     r = requests.get(_url(f'/feed/geo:{lat};{lng}/?token={TOKEN}'))
-    return r.json()
+    return r, r.json()
 
 
 # TODO(danoscarmike): what is the best format to pass a bounding box?
 # Is there a standard?
-def list_stations_in_bounds(lat1, lng1, lat2, lng2, raw=False):
+def get_stations_in_bounds(lat1, lng1, lat2, lng2):
     latlng = (',').join(list(map(str, [lat1, lng1, lat2, lng2])))
     r = requests.get(_url(f'/map/bounds/?token={TOKEN}&latlng={latlng}'))
-    return r.json()
+    return r, r.json()
 
 
 def search_stations(keyword):
     r = requests.get(_url(f'search/?keyword={keyword}&token={TOKEN}'))
-    return r.json()
+    return r, r.json()
