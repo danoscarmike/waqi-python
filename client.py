ddd0684cab8c5dd38bce7fc09f91276e3c6c6644
danoscarmike <omeara.dan@gmail.com>
2018-11-21 12:45:35 -0800

    return unadulterated

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 import requests
 import os
 
-from station import Station
-
 
 TOKEN = os.environ['AQIPY_TOKEN']
 
@@ -12,30 +10,28 @@ def _url(path):
 
 
 def get_city_feed(city):
-    return requests.get(_url(f'feed/{city}/?token={TOKEN}'))
+    r = requests.get(_url(f'feed/{city}/?token={TOKEN}'))
+    return r.json()
 
 
 def get_local_feed():
-    return requests.get(_url(f'feed/here/?token={TOKEN}'))
+    r = requests.get(_url(f'feed/here/?token={TOKEN}'))
+    return r.json()
 
 
 def get_feed_by_location(lat, lng):
-    return requests.get(_url(f'/feed/geo:{lat};{lng}/?token={TOKEN}'))
+    r = requests.get(_url(f'/feed/geo:{lat};{lng}/?token={TOKEN}'))
+    return r.json()
 
-# TODO(danoscarmike): what is the best format to pass a bounding box? Is there a standard?
+
+# TODO(danoscarmike): what is the best format to pass a bounding box?
+# Is there a standard?
 def list_stations_in_bounds(lat1, lng1, lat2, lng2, raw=False):
     latlng = (',').join(list(map(str, [lat1, lng1, lat2, lng2])))
     r = requests.get(_url(f'/map/bounds/?token={TOKEN}&latlng={latlng}'))
-
-    # TODO(danoscarmike): create a class for the response format??
-    if not raw:
-        for station in r.json()['data']:
-            r_city = get_city_feed('@'+str(station['uid']))
-            city_name = r_city.json()['data']['city']['name']
-            print(city_name)
-    else:
-        return r.json()
+    return r.json()
 
 
 def search_stations(keyword):
-    return requests.get(_url(f'search/?keyword={keyword}&token={TOKEN}'))
+    r = requests.get(_url(f'search/?keyword={keyword}&token={TOKEN}'))
+    return r.json()
