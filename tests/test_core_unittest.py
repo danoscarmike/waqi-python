40d78d6fb3cc6ac7caef140c5fff8c7a56e5ada7
danoscarmike <omeara.dan@gmail.com>
2018-11-26 23:12:20 -0800

    fix tests

--- tests/test_core_unittest.py
+++ tests/test_core_unittest.py
@@ -1,4 +1,6 @@
 from context import core
+from waqi_python import station
+
 
 import unittest
 
@@ -7,62 +9,44 @@ class TestCore(unittest.TestCase):
 
     def test_get_station_by_city_name(self):
         client = core.WaqiClient()
-        response, payload = client.get_station_by_city_name('shanghai')
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
-        self.assertTrue(len(payload['data']) > 0, 'Should contain data.')
+        response = client.get_station_by_city_name('shanghai')
+        self.assertTrue(isinstance(response, station.Station),
+                        'Should return instance of Station class')
 
     def test_get_local_station(self):
         client = core.WaqiClient()
-        response, payload = client.get_local_station()
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
+        response = client.get_local_station()
+        self.assertTrue(isinstance(response, station.Station),
+                        'Should return instance of Station class')
 
-    def test_get_station_by_latlon(self):
+    def test_get_station_by_latlng(self):
         # Pass arbitrary location (in Ireland)
         # Expect response['data'] to be populated
         client = core.WaqiClient()
-        response, payload = client.get_station_by_latlon(53.1, -7.4)
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
-        self.assertTrue(len(payload['data']) > 0, 'Should contain data.')
+        response = client.get_station_by_latlng(53.1, -7.4)
+        self.assertTrue(isinstance(response, station.Station),
+                        'Should return instance of Station class')
 
-    def test_list_stations_in_bounds(self):
+    def test_list_stations_by_bbox(self):
         # Pass arbitrary location (bounding box of New South Wales, Australia)
         # Expect response['data'] to be populated
         client = core.WaqiClient()
-        response, payload = client.list_stations_in_bounds(-37.6, 140.5,
-                                                        -27.9, 154.1)
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
-        self.assertTrue(len(payload['data']) > 0,
-                        'Payload should contain data.')
+        response = client.list_stations_by_bbox(-37.6, 140.5, -27.9, 154.1)
+        self.assertTrue(len(response) > 0, 'Payload should contain data.')
 
 
-    def test_get_stations_in_bounds_expect_empty(self):
+    def test_get_stations_by_bbox_expect_empty(self):
         # Pass arbitrary location (bounding box in Coral Sea)
         # Expect response['data'] to be empty
         client = core.WaqiClient()
-        response, payload = client.get_stations_in_bounds(-27.9, 154.1,
-                                                        -27.7, 154.2)
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
-        self.assertTrue(len(payload['data']) == 0, 'Payload should be empty.')
+        response = client.list_stations_by_bbox(-27.9, 154.1, -27.7, 154.2)
+        self.assertTrue(len(response) == 0, 'Payload should be empty.')
 
 
-    def test_search_stations(self):
+    def test_list_stations_by_keyword(self):
         client = core.WaqiClient()
-        response, payload = client.search_stations('san francisco')
-        self.assertEqual(response.status_code, 200, 'Should be 200.')
-        self.assertTrue('status' in payload, 'Payload should include "status".')
-        self.assertTrue('data' in payload, 'Payload should include "data".')
-        self.assertTrue(len(payload['data']) > 0,
-                        'Payload should contain data.')
+        response = client.list_stations_by_keyword('beijing')
+        self.assertTrue(len(response) > 0, 'Payload should contain data.')
 
 
 if __name__ == '__main__':
