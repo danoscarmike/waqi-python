53b7799b11449bbd1b3a1adde2be120ec871d87d
danoscarmike <omeara.dan@gmail.com>
2018-11-26 23:15:18 -0800

    deflake

--- waqi_python/core.py
+++ waqi_python/core.py
@@ -8,7 +8,6 @@ class WaqiClient(BaseClient):
     def _url(self, path):
         return self.base_url + path
 
-
     def get_station_by_city_name(self, city):
         r = requests.get(self._url(f'feed/{city}/'), params=self.params)
         # return r, r.json()
@@ -28,7 +27,6 @@ class WaqiClient(BaseClient):
         else:
             return 'Unknown Error'
 
-
     def get_station_by_latlng(self, lat, lng):
         r = requests.get(self._url(f'/feed/geo:{lat};{lng}/'),
                          params=self.params)
@@ -39,11 +37,11 @@ class WaqiClient(BaseClient):
         else:
             return 'Unknown Error'
 
-
     # TODO(danoscarmike): what is the best format to pass a bounding box?
     # Is there a standard?
     def list_stations_by_bbox(self, lat1, lng1, lat2, lng2, complete=False):
-        bbox = [min(lat1,lat2), min(lng1,lng2), max(lat1,lat2), max(lng1,lng2)]
+        bbox = [min(lat1, lat2), min(lng1, lng2), max(lat1, lat2),
+                max(lng1, lng2)]
         latlng = (',').join(list(map(str, bbox)))
         r = requests.get(self._url(f'/map/bounds/?latlng={latlng}'),
                          params=self.params)
@@ -58,7 +56,6 @@ class WaqiClient(BaseClient):
             return (r.json()['status'], r.json()['message'])
         else:
             return 'Unknown Error'
-
 
     def list_stations_by_keyword(self, keyword):
         r = requests.get(self._url(f'search/?keyword={keyword}'),
