c809df732579ea7e64bf515422887c2b2d2695b7
danoscarmike <omeara.dan@gmail.com>
2018-11-27 23:31:00 -0800

    more refactoring

--- waqi_python/core.py
+++ waqi_python/core.py
@@ -18,51 +18,49 @@ class WaqiClient():
     # def _url(self, path):
     #     return self.base_url + path
     def _get(self, url):
-        return requests.get(url, params=_PARAMS)
+        r = requests.get(url, params=_PARAMS)
+        if r.json()['status'] == 'ok':
+            return r.json()['data']
+        elif r.json()['status'] == 'error':
+            return None
+        else:
+            return None
 
 
     def get_station_by_path(self, path):
         url = _FEED_PATH_URL.format(path)
         r = self._get(url)
-        if r.json()['status'] == 'ok':
-            return Station(r.json()['data'])
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
+        if r:
+            return Station(r)
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
 
 
     def get_station_by_id(self, uid):
         url = _FEED_ID_URL.format(uid)
         r = self._get(url)
-        if r.json()['status'] == 'ok':
-            return Station(r.json()['data'])
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
+        if r:
+            return Station(r)
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
 
 
     def get_local_station(self):
         url = _FEED_LOCAL_URL
         r = self._get(url)
-        if r.json()['status'] == 'ok':
-            return Station(r.json()['data'])
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
+        if r:
+            return Station(r)
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
 
 
     def get_station_by_latlng(self, lat, lng):
         url = _FEED_GEO_URL.format(lat, lng)
         r = self._get(url)
-        if r.json()['status'] == 'ok':
-            return Station(r.json()['data'])
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
+        if r:
+            return Station(r)
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
 
 
     def list_stations_by_bbox(self, lat1, lng1, lat2, lng2, complete=False):
@@ -71,30 +69,27 @@ class WaqiClient():
         latlng = (',').join(list(map(str, bbox)))
         url = _MAP_BBOX_URL.format(latlng)
         r = self._get(url)
-        if r.json()['status'] == 'ok':
-            stations_locs = [Location(station) for station in r.json()['data']]
+        if r:
+            stations_locs = [Location(station) for station in r]
             if complete:
                 return [self.get_station_by_id(loc.uid)
                         for loc in stations_locs]
             else:
                 return stations_locs
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
+
 
     def list_stations_by_keyword(self, keyword, complete=False):
         url = _SEARCH_URL.format(keyword)
         r = self._get(url)
-        if r.json()['status'] == 'ok':
+        if r:
             stations = [(result['uid'],result['station']['name'])
-                        for result in r.json()['data']]
+                        for result in r]
             if complete:
                 return [self.get_station_by_id(station[0])
                         for station in stations]
             else:
                 return stations
-        elif r.json()['status'] == 'error':
-            return (r.json()['status'], r.json()['message'])
         else:
-            return 'Unknown Error'
+            print("I need to figure out error handling.")
